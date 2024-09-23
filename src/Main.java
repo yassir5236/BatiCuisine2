577b758b3756a4debc7d3078a36a9d2a5df3f825
yassir5236 <yassiraitlahmadi@gmail.com>
2024-09-23 14:15:45 +0100

    BAT-9 adding accepte or refuse devis for client functionality

--- src/Main.java
+++ src/Main.java
@@ -1,101 +1,145 @@
-//import controller.ClientController;
-//import controller.MainOuvreController;
-//import controller.MateriauController;
-//import controller.ProjetController;
-//import model.Projet;
 //
+//
+//import controller.*;
+//import service.ProjetService;
+//
+//import java.util.InputMismatchException;
 //import java.util.Scanner;
 //
-//
 //public class Main {
+//
 //    public static void main(String[] args) {
-//        ClientController obj = new ClientController();
-//        MateriauController obj2 = new MateriauController();
-//        MainOuvreController obj3 = new MainOuvreController();
-//
-//
-//
-//        System.out.println("===== Menu principale ======");
-//        System.out.println(" 1 - Créer un nouveau projet");
-//        System.out.println(" 2 - Afficher les projets existants ");
-//        System.out.println(" 3 - Calculer le coût d'un projet ");
-//        System.out.println(" 4 - Quitter \n");
-//        System.out.println(" Choisissez une option : ");
-//
-//
-//
-//
-//
-//
-//
+//        ClientController clientController = new ClientController();
+//        MateriauController materiauController = new MateriauController();
+//        MainOuvreController mainOuvreController = new MainOuvreController();
+//        CoutTotalController coutTotalController = new CoutTotalController();
+//        ProjetService projetService = new ProjetService();
+//        ProjetController projetController = new ProjetController();
 //        Scanner sc = new Scanner(System.in);
-//        int choice = sc.nextInt();
+//
+//        while (true) {
+//            afficherMenuPrincipal();
+//            int choice = lireChoix(sc);
+//            double TV;
+//
+//            switch (choice) {
+//                case 1:
+//                    gestionClient(sc);
+//                    break;
+//                case 2:
+//                    System.out.println("Affichage des projets existants ");
+//                    projetController.afficherTousLesProjets();
+//                    break;
+//                case 3:
+//                    boolean idValide = false;
+//
+//                    while (!idValide) {
+//                        try {
+//                            System.out.print("Entrez l'ID du projet ou tapez 0 pour retourner au menu précédent : ");
+//                            int idProjet = sc.nextInt();
+//
+//                                coutTotalController.coutTotal(idProjet);
+//                                idValide=true;
+//
+//
+//                            if (idProjet == 0) {
+//                                System.out.println("Retour au menu précédent...");
+//                                break;
+//                            }
 //
 //
 //
+//                        } catch (InputMismatchException e) {
+//                            System.out.println("Erreur : Veuillez entrer un ID de projet valide (nombre entier).");
+//                            sc.next();
+//                        }
+//                    }
+//                    break;
 //
-//        switch (choice) {
-//            case 1:
-//                GestionClient();
-//                break;
-//                case 7:
-//
-//                break;
-//            default:
-//                System.out.println("your chose is wronggg");
-//                break;
+//                case 4:
+//                    System.out.println("Quitter le programme...");
+//                    return;
+//                case 5:
+//                    int idprojet2 = sc.nextInt();
+//                    materiauController.addMateriau2(idprojet2);                    ;
+//                default:
+//                    System.out.println("Choix incorrect, veuillez réessayer.");
+//                    break;
+//            }
 //        }
-//
-//
 //    }
 //
-//    public static void GestionClient() {
-//        ClientController client = new ClientController();
-//        ProjetController projet = new ProjetController();
-//        MateriauController obj2 = new MateriauController();
-//        Scanner sc = new Scanner(System.in);
+//    private static void afficherMenuPrincipal() {
+//        System.out.println("===== Menu principal =====");
+//        System.out.println("1 - Créer un nouveau projet");
+//        System.out.println("2 - Afficher les projets existants");
+//        System.out.println("3 - Calculer le coût d'un projet");
+//        System.out.println("4 - Quitter");
+//        System.out.println("Choisissez une option : ");
+//    }
 //
-//        System.out.println("--- Recherche de client ---");
-//        System.out.println("Souhaitez-vous chercher un client existant ou en ajouter un nouveau ?\n");
-//        System.out.println("1 - Chercher un client existant ");
-//        System.out.println("2 - Ajouter un nouveau client ");
-//        System.out.println("\n");
-//        System.out.println(" Choisissez une option :");
-//
-//        int choice = sc.nextInt();
-//        switch (choice) {
-//            case 1:
-//                int idClient = client.recherchClient();
-//                System.out.println(idClient);
-//                System.out.println("Souhaitez-vous continuer avec ce client ? (true/false) : ");
-//                boolean response = sc.nextBoolean();
-//
-//                if (response) {
-//                     projet.addProjet(idClient);
-//
-//                } else {
-//                    System.out.println("Action annulée.");
-//                }
-//                break;
-//            case 2:
-//                int idNewClient = client.addClient();
-//                System.out.println(idNewClient);
-//                System.out.println("Souhaitez-vous continuer avec ce client ? (true/false) : ");
-//
-//                boolean response2 = sc.nextBoolean();
-//
-//                if (response2) {
-//                    projet.addProjet(idNewClient);
-//                } else {
-//                    System.out.println("Action annulée.");
-//                }
-//
-//            case 3:
+//    public static void gestionClient(Scanner sc) {
+//        ClientController clientController = new ClientController();
+//        ProjetController projetController = new ProjetController();
+//        CoutTotalController coutTotalController = new CoutTotalController();
 //
 //
+//        while (true) {
+//            System.out.println("--- Gestion des clients ---");
+//            System.out.println("1 - Chercher un client existant");
+//            System.out.println("2 - Ajouter un nouveau client");
+//            System.out.println("3 - retour au menu principal");
+//
+//            System.out.print("Choisissez une option : ");
+//
+//            int choice = lireChoix(sc);
+//
+//            switch (choice) {
+//                case 1:
+//                    int idClient = clientController.recherchClient();
+//                    System.out.print("Souhaitez-vous continuer avec ce client ? (true/false) : ");
+//                    if (sc.nextBoolean()) {
+//                        int idProjet = projetController.addProjet(idClient);
+//                        coutTotalController.coutTotal(idProjet);
+//                    } else {
+//                        System.out.println("Action annulée.");
+//                    }
+//                    return;
+//                case 2:
+//                    int idNewClient = clientController.addClient();
+//                    System.out.print("Souhaitez-vous continuer avec ce client ? (true/false) : ");
+//                    if (sc.nextBoolean()) {
+//                        projetController.addProjet(idNewClient);
+//                        coutTotalController.coutTotal(idNewClient);
+//                    } else {
+//                        System.out.println("Action annulée.");
+//                    }
+//                    return;
+//                case 3:
+//                    return;
+//                default:
+//                    System.out.println("Choix invalide. Veuillez réessayer.");
+//                    break;
+//            }
+//        }
+//    }
+//
+//    private static int lireChoix(Scanner sc) {
+//        int choix = -1;
+//        while (true) {
+//            try {
+//                choix = sc.nextInt();
+//                return choix;
+//            } catch (InputMismatchException e) {
+//                System.out.println("Saisie invalide, veuillez entrer un nombre.");
+//                sc.next();
+//            }
 //        }
 //    }
 //}
+
+
+
 
 
 import controller.*;
@@ -106,60 +150,36 @@ import java.util.Scanner;
 
 public class Main {
 
+    private static final ClientController clientController = new ClientController();
+    private static final MateriauController materiauController = new MateriauController();
+    private static final MainOuvreController mainOuvreController = new MainOuvreController();
+    private static final CoutTotalController coutTotalController = new CoutTotalController();
+    private static final ProjetService projetService = new ProjetService();
+    private static final ProjetController projetController = new ProjetController();
+
     public static void main(String[] args) {
-        ClientController clientController = new ClientController();
-        MateriauController materiauController = new MateriauController();
-        MainOuvreController mainOuvreController = new MainOuvreController();
-        CoutTotalController coutTotalController = new CoutTotalController();
-        ProjetService projetService = new ProjetService();
-        ProjetController projetController = new ProjetController();
         Scanner sc = new Scanner(System.in);
 
         while (true) {
             afficherMenuPrincipal();
             int choice = lireChoix(sc);
-            double TV;
 
             switch (choice) {
                 case 1:
                     gestionClient(sc);
                     break;
                 case 2:
-                    System.out.println("Affichage des projets existants ");
-                    projetController.afficherTousLesProjets();
+                    afficherProjetsExistants();
                     break;
                 case 3:
-                    boolean idValide = false;
-
-                    while (!idValide) {
-                        try {
-                            System.out.print("Entrez l'ID du projet ou tapez 0 pour retourner au menu précédent : ");
-                            int idProjet = sc.nextInt();
-
-                                coutTotalController.coutTotal(idProjet);
-                                idValide=true;
-
-
-                            if (idProjet == 0) {
-                                System.out.println("Retour au menu précédent...");
-                                break;
-                            }
-
-
-
-                        } catch (InputMismatchException e) {
-                            System.out.println("Erreur : Veuillez entrer un ID de projet valide (nombre entier).");
-                            sc.next();
-                        }
-                    }
+                    calculerCoutProjet(sc);
                     break;
-
                 case 4:
-                    System.out.println("Quitter le programme...");
-                    return;
+                    gestionDesDevis(sc);
+                    break;
                 case 5:
-                    int idprojet2 = sc.nextInt();
-                    materiauController.addMateriau2(idprojet2);                    ;
+                    quitterProgramme();
+                    break;
                 default:
                     System.out.println("Choix incorrect, veuillez réessayer.");
                     break;
@@ -172,47 +192,29 @@ public class Main {
         System.out.println("1 - Créer un nouveau projet");
         System.out.println("2 - Afficher les projets existants");
         System.out.println("3 - Calculer le coût d'un projet");
-        System.out.println("4 - Quitter");
-        System.out.println("Choisissez une option : ");
+        System.out.println("4 - Gestion des devis ");
+        System.out.println("5 - Quitter");
+        System.out.print("Choisissez une option : ");
     }
 
-    public static void gestionClient(Scanner sc) {
-        ClientController clientController = new ClientController();
-        ProjetController projetController = new ProjetController();
-        CoutTotalController coutTotalController = new CoutTotalController();
 
-
+    private static void gestionClient(Scanner sc) {
         while (true) {
             System.out.println("--- Gestion des clients ---");
             System.out.println("1 - Chercher un client existant");
             System.out.println("2 - Ajouter un nouveau client");
-            System.out.println("3 - retour au menu principal");
+            System.out.println("3 - Retour au menu principal");
 
             System.out.print("Choisissez une option : ");
-
             int choice = lireChoix(sc);
 
             switch (choice) {
                 case 1:
-                    int idClient = clientController.recherchClient();
-                    System.out.print("Souhaitez-vous continuer avec ce client ? (true/false) : ");
-                    if (sc.nextBoolean()) {
-                        int idProjet = projetController.addProjet(idClient);
-                        coutTotalController.coutTotal(idProjet);
-                    } else {
-                        System.out.println("Action annulée.");
-                    }
-                    return;
+                    chercherClient(sc);
+                    break;
                 case 2:
-                    int idNewClient = clientController.addClient();
-                    System.out.print("Souhaitez-vous continuer avec ce client ? (true/false) : ");
-                    if (sc.nextBoolean()) {
-                        projetController.addProjet(idNewClient);
-                        coutTotalController.coutTotal(idNewClient);
-                    } else {
-                        System.out.println("Action annulée.");
-                    }
-                    return;
+                    ajouterNouveauClient(sc);
+                    break;
                 case 3:
                     return;
                 default:
@@ -220,6 +222,71 @@ public class Main {
                     break;
             }
         }
+    }
+
+    private static void chercherClient(Scanner sc) {
+        int idClient = clientController.recherchClient();
+        System.out.print("Souhaitez-vous continuer avec ce client ? (true/false) : ");
+        if (sc.nextBoolean()) {
+            int idProjet = projetController.addProjet(idClient);
+            coutTotalController.coutTotal(idProjet);
+        } else {
+            System.out.println("Action annulée.");
+        }
+    }
+
+    private static void ajouterNouveauClient(Scanner sc) {
+        int idNewClient = clientController.addClient();
+        System.out.print("Souhaitez-vous continuer avec ce client ? (true/false) : ");
+        if (sc.nextBoolean()) {
+            int idProjet = projetController.addProjet(idNewClient);
+            coutTotalController.coutTotal(idProjet);
+        } else {
+            System.out.println("Action annulée.");
+        }
+    }
+
+    private static void afficherProjetsExistants() {
+        System.out.println("Affichage des projets existants...");
+        projetController.afficherTousLesProjets();
+    }
+
+    private static void calculerCoutProjet(Scanner sc) {
+        boolean idValide = false;
+
+        while (!idValide) {
+            try {
+                System.out.print("Entrez l'ID du projet ou tapez 0 pour retourner au menu précédent : ");
+                int idProjet = sc.nextInt();
+
+                if (idProjet == 0) {
+                    System.out.println("Retour au menu précédent...");
+                    return;
+                }
+
+                coutTotalController.coutTotal(idProjet);
+                idValide = true;
+
+            } catch (InputMismatchException e) {
+                System.out.println("Erreur : Veuillez entrer un ID de projet valide (nombre entier).");
+                sc.next();
+            }
+        }
+    }
+
+    private static void ajouterMateriauAuProjet(Scanner sc) {
+        try {
+            System.out.print("Entrez l'ID du projet pour ajouter un matériau : ");
+            int idProjet = sc.nextInt();
+            materiauController.addMateriau2(idProjet);
+        } catch (InputMismatchException e) {
+            System.out.println("Erreur : Veuillez entrer un ID de projet valide.");
+            sc.next(); // Pour consommer l'entrée invalide
+        }
+    }
+
+    private static void quitterProgramme() {
+        System.out.println("Quitter le programme...");
     }
 
     private static int lireChoix(Scanner sc) {
@@ -233,5 +300,27 @@ public class Main {
                 sc.next();
             }
         }
+    }
+
+
+
+    private static void gestionDesDevis(Scanner sc){
+        System.out.println("1- Afficher toutes les devis ");
+        System.out.println("2- Retur au menu principal ");
+        int choix = sc.nextInt();
+
+        switch (choix) {
+            case 1:
+                DevisController devisController = new DevisController();
+                devisController.DisplayAllDevis();
+                break;
+            case 2:
+                return;
+        }
+
+
+
+
+
     }
 }
